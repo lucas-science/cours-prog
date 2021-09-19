4573f890ab12b59aa6455d49d17e94c417d0f788
lucas-science <lucaslhomme01@gmail.com>
2021-09-19 18:47:47 +0200

    ex 82p37

--- maths/ex32p29.py
+++ maths/ex32p29.py
@@ -4,17 +4,14 @@ def liste_termes_u(n,a):
     u = a
     L = [u]
     for i in range(1,n+1):
-        u = 4*u-6
+        print(i)
+        u = (3*u)/(1+2*u)
         L.append(u)
     return L
 
-L1 = liste_termes_u(5,1)
-L2 = liste_termes_u(5,4)
-L3 = liste_termes_u(5,2)
+L1 = liste_termes_u(10,0.7)
 
-plot(L1, 'r.')
-plot(L2, 'b.')
-plot(L3, 'g.')
 
+plot(L1, 'b.')
 
 show()
